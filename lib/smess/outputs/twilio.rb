8cab4489e825804bef668f29330417f2a25312e5
Martin Westin <martin@eimermusic.com>
2013-09-19 13:17:33 +0200

    cleaned up Twilio handler

--- lib/smess/outputs/twilio.rb
+++ lib/smess/outputs/twilio.rb
@@ -4,70 +4,88 @@ module Smess
   class Twilio
     include Smess::Logging
 
-    attr_reader :sms
-
     def initialize(sms)
       @sms = sms
+      @results = []
     end
 
     def deliver
-      parts = Smess.separate_sms sms.message.strip_nongsm_chars
-      return false if parts[0].empty?
-
-      @client = ::Twilio::REST::Client.new(ENV["SMESS_TWILIO_SID"], ENV["SMESS_TWILIO_AUTH_TOKEN"])
-
-
-      results = []
-      while parts.length > 0
-        results << send_one_sms(sms, parts.shift)
+      parts.each do |part|
+        results << send_one_sms(part)
       end
-      results[0][:data][:text] = sms.message.strip_nongsm_chars
-      results[0]
+
+      # we don't actually return the status for any of additional messages which is cheating
+      results.first
     end
 
-    def send_one_sms(sms, message)
+    private
+
+    attr_reader :sms
+    attr_accessor :results
+
+    def from
+      ENV["SMESS_TWILIO_FROM"]
+    end
+
+    def parts
+      @parts ||= split_parts
+    end
+
+    def split_parts
+      Smess.split_sms(sms.message.strip_nongsm_chars).reject {|s| s.empty? }
+    end
+
+    def client
+      @client ||= ::Twilio::REST::Client.new(ENV["SMESS_TWILIO_SID"], ENV["SMESS_TWILIO_AUTH_TOKEN"])
+    end
+
+    def send_one_sms(message)
       begin
-        response = @client.account.sms.messages.create({
-          from: ENV["SMESS_TWILIO_FROM"],
+        response = client.account.sms.messages.create({
+          from: from,
           to: "+#{sms.to}",
           body: message,
           status_callback: ENV["SMESS_TWILIO_CALLBACK_URL"]
         })
+        result = normal_result(response)
       rescue Exception => e
-        logger.warn response
-        # connection problem or some error
-        result = {
-          response_code: '-1',
-          response: {
-            temporaryError: 'true',
-            responseCode: e.code,
-            responseText: e.message
-          },
-          data: {
-            to: sms.to,
-            text: message,
-            from: ENV["SMESS_TWILIO_FROM"]
-          }
-        }
-      else
-        response_code = response.status
-        response_code = "0" unless response.status == "failed"
-        # Successful response
-        result = {
-          message_id: response.sid,
-          response_code: response_code.to_s,
-          response: MultiJson.load(@client.last_response.body),
-          destination_address: sms.to,
-          data: {
-            to: sms.to,
-            text: message,
-            from: ENV["SMESS_TWILIO_FROM"]
-          }
-        }
+        result = result_for_error(e)
       end
       result
     end
 
+    def normal_result(response)
+      response_code = response.status
+      response_code = "0" unless response.status == "failed"
+      # Successful response
+      {
+        message_id: response.sid,
+        response_code: response_code.to_s,
+        response: MultiJson.load(client.last_response.body),
+        destination_address: sms.to,
+        data: result_data
+      }
+    end
+
+    def result_for_error(e)
+      {
+        response_code: '-1',
+        response: {
+          temporaryError: 'true',
+          responseCode: '-1',
+          responseText: e.message
+        },
+        data: result_data
+      }
+    end
+
+    def result_data
+      {
+        to: sms.to,
+        text: sms.message.strip_nongsm_chars,
+        from: from
+      }
+    end
 
   end
 end
