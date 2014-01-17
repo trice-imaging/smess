6cf66bd9aa23008fd08939a132599d1162877a63
Martin Westin <martin@eimermusic.com>
2014-01-17 09:42:33 +0100

    iConective carrier adaptation update

--- lib/smess/outputs/iconectiv.rb
+++ lib/smess/outputs/iconectiv.rb
@@ -7,5 +7,11 @@ module Smess
       "ICONECTIV"
     end
 
+    # iConectiv asserts that this is all that is still reqquired and that it wont break other carriers.
+    # test of major carriers confirm this too... let's see how the small ones do.
+    def perform_operator_adaptation(msisdn)
+      adapt_for_t_mobile_us msisdn
+    end
+
   end
 end
